3a82b9fadd2d0fa7511be1e9d0f07a77910413e6
Mahmood <pejvakco@gmail.com>
2021-04-05 01:06:31 +0200

    Add enter key eventlistener

--- src/listeners/estimateNationality.listener.js
+++ src/listeners/estimateNationality.listener.js
@@ -5,5 +5,12 @@ import { estimateNationality } from '../handlers/estimateNationality.handler.js'
 const estimateNationalityButton = document.querySelector(
   '.estimate-nationality-button',
 );
+const firstNameInput = document.querySelector('#first_name_input');
 
 estimateNationalityButton.addEventListener('click', estimateNationality);
+
+firstNameInput.addEventListener('keypress', function (e) {
+  if (e.key === 'Enter') {
+    estimateNationality();
+  }
+});
